5ca52054db05647748f6cf7123213eb78e46f7c6
Sarah Shafqat <51702673+sarahshafqat96@users.noreply.github.com>
2024-07-27 14:47:18 +0500

    Update createcar.py

--- code/createcar.py
+++ code/createcar.py
@@ -13,17 +13,17 @@ class CreateCar:                                                              #C
         self.car_speed = STARTING_MOVE_DISTANCE                               #Variable to store car speed
 
     def generate_car(self):
-        y_pos = random.randint(-250, 270)                                   #Y-coordinate will be chosen randomly from this range
-        while y_pos in range(65, 135) or y_pos in range(-130, -65):            #Y-coordinate should not fall in these ranges because it will create the car on white lines of the road
+        y_pos = random.randint(-250, 270)                                     #Y-coordinate will be chosen randomly from this range
+        while y_pos in range(65, 135) or y_pos in range(-130, -65):           #Y-coordinate should not fall in these ranges because it will create the car on white lines of the road
             y_pos = random.randint(-270, 270)
-        car_prob = random.randint(1, 6)                                 #Only create a car when random_chance = 1
+        car_prob = random.randint(1, 6)                                       #Only create a car when random_chance = 1
         if car_prob == 1:
             new_car = Turtle()                                                #The car will be an object of class Turtle will be square
             new_car.penup()                                                   #The car/turtle should not leave nay traces
             turtle_shape = random.choice(car_list)                            #The shape will be selected randomly from "car_list" initialized above
             new_car.screen.addshape(turtle_shape)                             #That shape will be added to the screen
             new_car.shape(turtle_shape)                                       #And assigned to the turtle
-            new_car.goto(340, y_pos)                                       #The car/turtle will go to these co-ordinates
+            new_car.goto(340, y_pos)                                          #The car/turtle will go to these co-ordinates
             self.all_cars.append(new_car)                                     #Add that car to the all-cars list
 
     def move_car(self):
