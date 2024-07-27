28942f3436502c9edbb7e8c632dac0d07bf17edf
Sarah Shafqat <51702673+sarahshafqat96@users.noreply.github.com>
2024-07-27 14:47:41 +0500

    Update scoreboard.py

--- code/scoreboard.py
+++ code/scoreboard.py
@@ -6,7 +6,7 @@ class Score(Turtle):                                        #Creating Score clas
         super().__init__()                                  #Turtle as its super class
         self.hideturtle()                                   #Hide the turtle
         self.penup()                                        #Turtle should not leave trace lines
-        self.goto(-300, 320)                             #Move the score/turtle to top left on the screen
+        self.goto(-300, 320)                                #Move the score/turtle to top left on the screen
         self.color("black")                                 #Color will be black
         self.score_total = 0                                #Initially the score will be zero
         self.level = 0                                      #Initially the level will be zero
